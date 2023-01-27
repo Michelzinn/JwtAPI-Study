7ae41d75bc30f1e1675bcd87b7393daed6cffe0c
Michel <michelbreno5@hotmail.com>
2023-01-27 17:39:16 -0300

    Metodo de login e rota de login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,29 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      token = encode_token({user_id: @user.id})
-      render json: { user: @user, token: token}, 
-      status: :ok
+      token = encode_token({user_id: @user.id}) # metodo JWT.encode herdado do  app controller
+      render json: { user: @user, token: token}, status: :ok
     else
-      render json: {error: 'Usuário ou senha inválidos'}, 
-      status: :unprocessable_entity
+      render json: {error: 'Usuário ou senha inválidos'}, status: :unprocessable_entity
     end
   end
 
-private
+  def login
+    @user = User.find_by(username: user_params[:username])
+
+    if @user && @user.authenticate(user_params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: { user: @user, token: token}, status: :ok
+    else
+      render json: {error: 'Usuário ou senha inválidos'}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
   def user_params
     params.permit(
-      :username, 
+      :username,
       :password
     )
   end
