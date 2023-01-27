9868cbaf5eec8da910f2b8c0558c8ccdf486a813
Michel <michelbreno5@hotmail.com>
2023-01-27 16:57:55 -0300

    Criacao das rotas e controllers da api

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,21 @@
+class UserController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: { user: @user, token: token }, 
+      status: :ok
+    else
+      render json: {error: 'Usuário ou senha inválidos'}, 
+      status: :unprocessable_entity
+    end
+  end
+
+private
+  def user_params
+    params.permit(
+      :username, 
+      :password
+    )
+  end
+end
