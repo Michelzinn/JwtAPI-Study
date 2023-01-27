d6406d0934b7b19c418bd84ed58db01a56de1bae
Michel <michelbreno5@hotmail.com>
2023-01-27 17:22:55 -0300

    fix nome do controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
       token = encode_token({user_id: @user.id})
-      render json: { user: @user, token: token }, 
+      render json: { user: @user, token: token}, 
       status: :ok
     else
       render json: {error: 'Usuário ou senha inválidos'}, 
